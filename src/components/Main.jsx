ada531a2474e3cafd28080ba1dd5b3046fdc8ff6
faizshaikh17 <mohdfaiz8101@gmail.com>
2025-04-18 01:11:40 +0530

    finally json data handled

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,33 +1,68 @@
 import React, { useEffect, useState } from 'react'
 import { fetchTopStories, fetchItemsById } from '../utils/utils'
 
-export default function Main() {
 
-    const [topStoriesIds, setTopStoriesIds] = useState([])
-    const [top50Stories, setTop50Stories] = useState([])
-
-    useEffect(() => {
-        const loadStories = async () => {
-            const res = await fetchTopStories();
-            setTopStoriesIds(res)
+const fetchStories = async () => {
+    try {
+        const storyIds = await fetchTopStories()
+        if (!storyIds) {
+            throw new Error("no response");
         }
-        loadStories()
-
-        const top50 = topStoriesIds.slice(0, 10);
-
-        top50.forEach(async (id) => {
-            const res = await fetchItemsById(id);
-            setTop50Stories(res);
+        const stories = storyIds.slice(0, 10).map(async (id) => {
+            const story = await fetchItemsById(id);
+            if (!story) {
+                throw new Error("no response");
+            }
+            return {
+                'url': story.url,
+                'by': story.by,
+                'comments': story.comments,
+                'score': story.score,
+                'time': story.time,
+            }
         })
 
-        console.log(top50);
+        return Promise.all(stories);
 
+    } catch (error) {
+        console.error('Error fetching top stories:', error);
+        return [];
+    }
+}
+
+
+export default function Main() {
+    const [topStories, setTopStories] = useState([]);
+    useEffect(() => {
+        fetchStories().then(newStories => setTopStories(prev => [...prev, ...newStories]))
     }, [])
-
-
 
     return (
         <main className='min-h-screen'>
+            <div>
+                {
+                    topStories.map(item => (
+                        <>
+                            <p></p>
+                        </>
+                    ))
+                }
+            </div>
         </main>
     )
 }
+
+
+
+
+// {
+//     by: "mfiguiere",
+//     descendants: 17,
+//     id: 43716856,
+//     kids: (9)[…],
+//     score: 32,
+//     time: 1744897909,
+//     title: "OpenAI looked at buying Cursor creator before turning to Windsurf",
+//     type: "story",
+//     url: "https://www.cnbc.com/2025/04/17/openai-looked-at-cursor-before-considering-deal-with-rival-windsurf.html"
+// }
