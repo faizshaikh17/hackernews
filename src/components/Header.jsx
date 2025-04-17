83c3d4564d0111f253eb96a5347b593b4e48f248
faizshaikh17 <mohdfaiz8101@gmail.com>
2025-04-18 02:52:45 +0530

    frontend looks good

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 export default function Header() {
     return (
         <>
-            <header className='py-4 text-[#FC7D49]'>
+            <header className='p-4 text-[#FC7D49]'>
                 <div className='flex items-center justify-between'>
                     <span className='font-semibold text-xl'>Hack&Hacker</span>
                     <span className='font-semibold text-xl'>github</span>
--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -8,12 +8,15 @@ const fetchStories = async () => {
         if (!storyIds) {
             throw new Error("no response");
         }
-        const stories = storyIds.slice(0, 10).map(async (id) => {
+        const stories = storyIds.slice(0, 5).map(async (id) => {
             const story = await fetchItemsById(id);
             if (!story) {
                 throw new Error("no response");
             }
+
             return {
+                'id': story.id,
+                'title': story.title,
                 'url': story.url,
                 'by': story.by,
                 'comments': story.comments,
@@ -34,16 +37,29 @@ const fetchStories = async () => {
 export default function Main() {
     const [topStories, setTopStories] = useState([]);
     useEffect(() => {
-        fetchStories().then(newStories => setTopStories(prev => [...prev, ...newStories]))
+        fetchStories().then(newStories => setTopStories(prev => {
+            const unique = newStories.filter(newStory => !prev.some(prevStory => newStory.id === prevStory.id))
+            return [...prev, ...unique]
+        }
+        ))
     }, [])
-
+    console.log(topStories)
     return (
-        <main className='min-h-screen'>
-            <div>
+        <main className='min-h-screen my-4'>
+            <div className='space-y-5 '>
                 {
                     topStories.map(item => (
                         <>
-                            <p></p>
+                            <div className='spacye-y-4'>
+                                <div className='space-y-3 p-4 hover:bg-[#171717]'>
+                                    <a href={item.url} className=' hover:text-[#FC7D49] font-semibold hover:underline sm:text-lg text-base'>{item.title}</a>
+                                    <p className='text-xs sm:text-sm'>by <span className='text-[#FC7D49] underline'>{item.by}</span></p>
+                                    <span>{item.score}|{item.time}</span>
+                                    <br />
+                                </div>
+                                <div className='bg-[#171717] mt-5 h-0.5'></div>
+                            </div>
+
                         </>
                     ))
                 }
