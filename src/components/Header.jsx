8628f7946a0f6440680954ca3a452c7384c08aa4
faizshaikh17 <mohdfaiz8101@gmail.com>
2025-04-19 13:17:03 +0530

    ui fixes

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -4,7 +4,7 @@ import { Link } from 'react-router-dom'
 export default function Header() {
     return (
         <>
-            <header className='p-4 tracking-tight text-[#121212]'>
+            <header className='p-5 tracking-tight text-[#121212]'>
                 <div className='flex justify-between items-center'>
                     <Link to={'/'}>
                         <span className='text-[1.2rem] font-semibold  text-[#121212] sm:text-[1.4rem]'>TheDaily<span className='text-[#121212] hover:text-[#121212]'>Hacker</span></span>
