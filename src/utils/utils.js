6f89e8e218fd08f6bfed18d12a6330ca138c5d47
faizshaikh17 <mohdfaiz8101@gmail.com>
2025-04-19 06:21:42 +0530

    api calls, axios added

--- src/utils/utils.js
+++ src/utils/utils.js
@@ -1,17 +1,20 @@
+import axios, { Axios } from "axios"
+
+
 export const fetchTopStories = async () => {
-    const res = await fetch('https://hacker-news.firebaseio.com/v0/topstories.json?print=pretty')
+    const res = await axios.get('https://hacker-news.firebaseio.com/v0/topstories.json?print=pretty')
     const data = await res.json()
     return data
 }
 
 export const fetchItemsById = async (id) => {
-    const res = await fetch(`https://hacker-news.firebaseio.com/v0/item/${id}.json?print=pretty`)
+    const res = await axios.get(`https://hacker-news.firebaseio.com/v0/item/${id}.json?print=pretty`)
     const data = await res.json()
     return data
 }
 
 export const fetchUserById = async (username) => {
-    const res = await fetch(`https://hacker-news.firebaseio.com/v0/user/${username}.json?print=pretty`)
+    const res = await axios.get(`https://hacker-news.firebaseio.com/v0/user/${username}.json?print=pretty`)
     const data = await res.json()
     return data
 }
