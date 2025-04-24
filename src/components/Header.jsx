f0831c5cedbd1ba0a737efca1db3e6a0c19071d4
faizshaikh17 <mohdfaiz8101@gmail.com>
2025-04-24 18:20:58 +0530

    fix: css title colors

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -18,7 +18,7 @@ export default function Header() {
             <header className='p-5 tracking-tight bg-[#f0f0f0] dark:bg-[#121212] transition-colors duration-300'>
                 <div className='flex justify-between items-center'>
                     <Link to={'/'}>
-                        <span className='text-[1.2rem] text-[#FA7921] dark:text-[#f0f0f0] font-semibold sm:text-[1.4rem]'>TheDailyHacker</span>
+                        <span className='text-[1.2rem] text-[#FA7921] dark:text-[#f0f0f0] font-semibold sm:text-[1.4rem]'>TheDaily<span className='dark:text-[#FA7921]'>Hacker</span></span>
                     </Link>
                     <span className='flex justify-between items-center gap-2'>
                         <span className='dark:text-[#FA7921] text-[#121212] dark:hover:text-[#FE9920] underline underline-offset-8 font-light text-lg'><a href="https://github.com/faizshaikh17/newhackertimes" target="_blank" rel="noopener noreferrer">github</a>
